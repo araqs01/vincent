953338b3741d68045455c4a1f19f4a2023001f1b
araqs01 <araxiazurabyan001@gmail.com>
2025-11-07 01:06:41 +0400

    test

--- config/livewire.php
+++ config/livewire.php
@@ -64,17 +64,17 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+        'disk' => 'local',       // хранить временные файлы в /storage/app/livewire-tmp
+        'rules' => ['required', 'file', 'mimes:xlsx,csv', 'max:51200'], // 50MB
+        'directory' => 'livewire-tmp',
+        'middleware' => ['throttle:60,1'], // стандартный лимит Livewire
+        'preview_mimes' => [
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
-        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
+        'max_upload_time' => 15, // до 15 минут на загрузку
+        'cleanup' => true,
     ],
 
     /*
